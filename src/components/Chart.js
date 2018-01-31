fcb5aa3e8726268e1088791872c24edafc66b3e0
Manuel Baronetti <manuelbaronetti@gmail.com>
2018-01-30 21:56:16 -0300

    stats v2

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -4,13 +4,13 @@ import { LineChart, Line, ResponsiveContainer } from 'recharts';
 class Chart extends Component {
     render() {
         const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+      {name: 'Page A', uv: 4000 * Math.random(), pv: 2400 * Math.random(), amt: 2400},
+      {name: 'Page B', uv: 3000 * Math.random(), pv: 1398 * Math.random(), amt: 2210},
+      {name: 'Page C', uv: 2000 * Math.random(), pv: 9800 * Math.random(), amt: 2290},
+      {name: 'Page D', uv: 2780 * Math.random(), pv: 3908 * Math.random(), amt: 2000},
+      {name: 'Page E', uv: 1890 * Math.random(), pv: 4800 * Math.random(), amt: 2181},
+      {name: 'Page F', uv: 2390 * Math.random(), pv: 3800 * Math.random(), amt: 2500},
+      {name: 'Page G', uv: 3490 * Math.random(), pv: 4300 * Math.random(), amt: 2100},
     ];
     return (
         <ResponsiveContainer>
